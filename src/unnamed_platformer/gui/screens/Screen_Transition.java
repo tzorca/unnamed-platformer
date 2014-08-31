57cb0bede81cc2ccffd1635d27d30abfe68708f8
tzorca <tzorca@users.noreply.github.com>
2014-08-30 21:00:43 -0400

    Committed file that was missed in a recent commit.

---
+++ src/unnamed_platformer/gui/screens/Screen_Transition.java
@@ -0,0 +1,50 @@
+package unnamed_platformer.gui.screens;
+
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import unnamed_platformer.game.other.World;
+import unnamed_platformer.gui.GUIHelper;
+import unnamed_platformer.gui.GUIManager;
+
+public class Screen_Transition extends BaseScreen_GUI
+{
+	// Instance initializer
+	{
+		pnlSurface.setBackground(GUIManager.GUI_BG_COLOR);
+
+		Label lblNextLevel = new Label("Level "
+				+ String.valueOf(World.getCurrentLevelIndex() + 1));
+		lblNextLevel.setFont(GUIManager.HEADING_FONT);
+		lblNextLevel.setForeground(GUIManager.GUI_FG_COLOR);
+
+		JButton btnStart = new JButton("Start");
+		btnStart.setFont(GUIManager.SUB_HEADING_FONT);
+		GUIHelper.styleButton(btnStart, 12);
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				switch (GUIManager.getPreviousScreen()) {
+				case Edit:
+				case Play:
+					GUIManager.back();
+					break;
+				case SelectWorld:
+				case Start:
+				case Transition:
+				default:
+					// ?
+					break;
+				}
+			}
+		});
+		
+		
+		pnlSurface.add(lblNextLevel, GUIManager.CENTER_LAYOUT
+				+ ", gapbottom 20%, ");
+		pnlSurface.add(btnStart, GUIManager.CENTER_LAYOUT);
+
+	}
+}
