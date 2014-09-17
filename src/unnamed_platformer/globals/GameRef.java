1f290c82b8ea59baa786c0b02cffe873a06e89b0
tzorca <tzorca@users.noreply.github.com>
2014-09-16 21:43:24 -0400

    Tweaked physics to make player move a bit faster and jump a bit higher.

--- src/unnamed_platformer/globals/GameRef.java
+++ src/unnamed_platformer/globals/GameRef.java
@@ -5,14 +5,14 @@ public final class GameRef
 
 	public static final float DEFAULT_SPRING_STRENGTH = 2f;
 
-	public static final float DEFAULT_PLR_ACCELERATION = 1f;
+	public static final float DEFAULT_PLR_ACCELERATION = 0.8f;
 	public static final float DEFAULT_PLR_DECELERATION = 3f;
-	public static final float DEFAULT_PLR_MAX_SPEED = 5f;
+	public static final float DEFAULT_PLR_MAX_SPEED = 6f;
 
 	public static final float DEFAULT_PLR_JUMP_STRENGTH = 8f;
 
-	public static final float DEFAULT_SHOOT_SPEED = 0.9f;
-	public static final float DEFAULT_SHOOT_DELAY = 100;
+	public static final float DEFAULT_SHOOT_SPEED = 0.7f;
+	public static final float DEFAULT_SHOOT_DELAY = 150;
 
 	public final static float DEFAULT_WATER_SPEED_FACTOR = 0.5f;
 	public final static float DEFAULT_FAST_REGION_SPEED_FACTOR = 2f;
