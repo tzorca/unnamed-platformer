bb662e7cfa8f3c5b88aff88246527f60ff76d61e
tzorca <tzorca@users.noreply.github.com>
2014-12-26 15:13:50 -0500

    In transition screen, replaced GUI button with countdown timer

--- src/unnamed_platformer/view/gui/screens/Screen_Transition.java
+++ src/unnamed_platformer/view/gui/screens/Screen_Transition.java
@@ -1,67 +1,72 @@
 package unnamed_platformer.view.gui.screens;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Collection;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
+import unnamed_platformer.app.InputManager;
+import unnamed_platformer.app.InputManager.PlrGameKey;
+import unnamed_platformer.app.TimeManager;
 import unnamed_platformer.game.other.World;
 import unnamed_platformer.globals.StyleRef;
 import unnamed_platformer.res_mgt.SoundManager;
-import unnamed_platformer.view.gui.GUIHelper;
 import unnamed_platformer.view.gui.GUIManager;
 import unnamed_platformer.view.gui.GUIManager.ScreenType;
 
-import com.google.common.collect.Lists;
-
 public class Screen_Transition extends BaseScreen_GUI
 {
-	JButton btnNext = new JButton("Start");
+	JLabel lblInfo, lblTimer = new JLabel();
+
+	private long timeStarted;
+	private ScreenType nextScreen;
+
+	private final int TRANSITION_SECONDS = 3;
 
 	public Screen_Transition() {
 		super();
 
-		JLabel lblInfo;
+		timeStarted = TimeManager.time();
+		lblTimer.setText(String.valueOf(TRANSITION_SECONDS));
 
-		// This will already have been set by the end level object
 		int nextLevelIndex = World.getCurrentLevelIndex();
 
 		if (World.hasLevelIndex(nextLevelIndex)) {
 			// Play a sound for completing the level!
 			SoundManager.playSample("level-complete");
 
-			lblInfo = new JLabel("Level "
-					+ String.valueOf(World.getCurrentLevelIndex() + 1));
-
-			btnNext.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					GUIManager.changeScreen(ScreenType.Play);
-				}
-			});
+			lblInfo = new JLabel("Next: Level "
+					+ String.valueOf(nextLevelIndex + 1));
+			nextScreen = ScreenType.Play;
 		} else {
 			lblInfo = new JLabel("You finished all the levels! Good job!");
-
-			btnNext = new JButton("Ok");
-			btnNext.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					GUIManager.changeScreen(ScreenType.SelectWorld);
-				}
-			});
+			nextScreen = ScreenType.SelectWorld;
 		}
 
 		StyleRef.STYLE_HEADING.apply(lblInfo);
-		StyleRef.STYLE_NORMAL_BUTTON.apply(btnNext);
-		
+		StyleRef.STYLE_HEADING.apply(lblTimer);
+
 		pnlSurface.add(lblInfo, StyleRef.CENTER_LAYOUT + ", gaptop 10%, ");
-		pnlSurface.add(btnNext, StyleRef.CENTER_LAYOUT + ", pushy");
+		pnlSurface.add(lblTimer, StyleRef.CENTER_LAYOUT + ", gaptop 10%, ");
 
-		GUIHelper.addDirectionalNavigation(Lists.newArrayList(btnNext), null);
-
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				btnNext.requestFocusInWindow();
+		pnlSurface.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				Collection<PlrGameKey> plrGameKeys = InputManager
+						.getGameKeysMatchingKeyEvent(e);
+				for (PlrGameKey plrGameKey : plrGameKeys) {
+					switch (plrGameKey.getGameKey()) {
+					case A:
+						GUIManager.changeScreen(nextScreen);
+						break;
+					case B:
+						// TODO
+						break;
+					default:
+						break;
+					}
+				}
 			}
 		});
 	}
@@ -69,9 +74,19 @@ public class Screen_Transition extends BaseScreen_GUI
 	public void update() {
 		super.update();
 
-		// Default button selection
-		if (!btnNext.hasFocus()) {
-			btnNext.requestFocusInWindow();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				pnlSurface.requestFocusInWindow();
+			}
+		});
+
+		int timeElapsed = (int) TimeManager.secondsSince(timeStarted);
+		int timeLeft = TRANSITION_SECONDS - timeElapsed;
+
+		lblTimer.setText(String.valueOf(timeLeft));
+
+		if (timeLeft <= 0) {
+			GUIManager.changeScreen(nextScreen);
 		}
 	}
 }
