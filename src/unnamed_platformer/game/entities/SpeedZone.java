882ef7d8781da67542873202ac2fc185c0871e8f
tzorca <tzorca@users.noreply.github.com>
2015-02-26 16:42:14 -0500

    Removed a few unneeded Interaction classes.

--- src/unnamed_platformer/game/entities/SpeedZone.java
+++ src/unnamed_platformer/game/entities/SpeedZone.java
@@ -2,16 +2,18 @@ package unnamed_platformer.game.entities;
 
 import org.newdawn.slick.Color;
 
-import unnamed_platformer.game.behaviours.Inter_FastRegion;
+import unnamed_platformer.game.behaviours.Inter_VelocityMultiplier;
 import unnamed_platformer.game.other.EntitySetup;
+import unnamed_platformer.globals.GameGlobals;
 
-public class SpeedZone extends ActiveEntity {
-
+public class SpeedZone extends ActiveEntity
+{
 	public SpeedZone(EntitySetup entitySetup) {
 		super(entitySetup);
 
-		interactions.add(new Inter_FastRegion());
+		interactions.add(new Inter_VelocityMultiplier(
+				GameGlobals.FAST_REGION_SPEED_MODIFIER));
 		zIndex = 3;
-		graphic.color = new Color(1,1,1,0.5f);
+		graphic.color = new Color(1, 1, 1, 0.5f);
 	}
 }
--- src/unnamed_platformer/game/entities/Water.java
+++ src/unnamed_platformer/game/entities/Water.java
@@ -2,16 +2,19 @@ package unnamed_platformer.game.entities;
 
 import org.newdawn.slick.Color;
 
-import unnamed_platformer.game.behaviours.Inter_SlowRegion;
+import unnamed_platformer.game.behaviours.Inter_VelocityMultiplier;
 import unnamed_platformer.game.other.EntitySetup;
+import unnamed_platformer.globals.GameGlobals;
 
-public class Water extends ActiveEntity {
+public class Water extends ActiveEntity
+{
 
 	public Water(EntitySetup entitySetup) {
 		super(entitySetup);
 
-		interactions.add(new Inter_SlowRegion());
+		interactions.add(new Inter_VelocityMultiplier(
+				GameGlobals.WATER_SPEED_MODIFIER));
 		zIndex = 3;
-		graphic.color = new Color(1,1,1,0.5f);
+		graphic.color = new Color(1, 1, 1, 0.5f);
 	}
 }
